3aab31c7bc7974f27a4bc3ed7405d8de7df27357
esau-morais <esaumorais7@gmail.com>
2024-04-24 12:15:41 -0300

    fix: shouldnt render partykit host as server env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,16 +5,19 @@ export const env = createEnv({
   server: {
     TURSO_DATABASE_URL: z.string().url().min(1),
     TURSO_AUTH_TOKEN: z.string().min(1),
-    PARTYKIT_HOST: z.string().min(1),
     SESSION_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_LIVEBLOCKS_API_KEY: z.string().min(1),
+    NEXT_PUBLIC_PARTYKIT_HOST: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_LIVEBLOCKS_API_KEY: process.env.NEXT_PUBLIC_LIVEBLOCKS_API_KEY,
+    NEXT_PUBLIC_PARTYKIT_HOST: process.env.NEXT_PUBLIC_PARTYKIT_HOST,
   },
 });
 
-const PROTOCOL = env.PARTYKIT_HOST.startsWith("127.0.0.1") ? "http" : "https";
-export const PARTYKIT_URL = `${PROTOCOL}://${env.PARTYKIT_HOST}`;
+const PROTOCOL = env.NEXT_PUBLIC_PARTYKIT_HOST.startsWith("127.0.0.1")
+  ? "http"
+  : "https";
+export const PARTYKIT_URL = `${PROTOCOL}://${env.NEXT_PUBLIC_PARTYKIT_HOST}`;
